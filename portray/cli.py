97e3e5e0929e9308bc46de7e6d9c04cf0ef097ea
Christo De Lange <christo.delange@sands.com>
2025-05-28 13:30:08 -0400

    fix: typer annotations for 0.11

--- portray/cli.py
+++ portray/cli.py
@@ -13,6 +13,7 @@ definition in the [API module](/reference/portray/api)
 
 import sys
 from pprint import pprint
+from typing import List, Optional
 
 import typer
 
@@ -75,7 +76,7 @@ def as_html(
         False,
         help="If set to True any existing documentation output will be removed before generating new documentation.",
     ),
-    modules: list[str] | None = opt_modules,
+    modules: Optional[List[str]] = opt_modules,
 ) -> None:
     """Produce HTML documentation for a Python project placing it into output_dir."""
     api.as_html(
@@ -95,8 +96,8 @@ app.command(name="as-html")(as_html)
 def project_configuration(
     directory: str = typer.Argument("", help="The root folder of your project."),
     config_file: str = typer.Option("pyproject.toml", help="The TOML formatted config file you wish to use."),
-    modules: list[str] | None = opt_modules,
-    output_dir: str | None = typer.Option(None, help="The directory to place the generated HTML into."),
+    modules: Optional[List[str]] = opt_modules,
+    output_dir: Optional[str] = typer.Option(None, help="The directory to place the generated HTML into."),
 ) -> None:
     """Return the configuration associated with a project."""
     config = api.project_configuration(
@@ -120,9 +121,9 @@ def server(
         False,
         help="If true a browser will be opened pointing at the documentation server",
     ),
-    port: int | None = typer.Option(None, help="The port to expose your documentation on (defaults to: 8000)"),
-    host: str | None = typer.Option(None, help="The host to expose your documentation on (defaults to 127.0.0.1)"),
-    modules: list[str] | None = opt_modules,
+    port: Optional[int] = typer.Option(None, help="The port to expose your documentation on (defaults to: 8000)"),
+    host: Optional[str] = typer.Option(None, help="The host to expose your documentation on (defaults to 127.0.0.1)"),
+    modules: Optional[List[str]] = opt_modules,
     reload: bool = typer.Option(False, help="If true the server will live load any changes"),
 ) -> None:
     """Run a development webserver enabling you to browse documentation locally."""
@@ -141,9 +142,9 @@ def server(
 def in_browser(
     directory: str = typer.Argument("", help="The root folder of your project."),
     config_file: str = typer.Option("pyproject.toml", help="The TOML formatted config file you wish to use."),
-    port: int | None = typer.Option(None, help="The port to expose your documentation on (defaults to: 8000)"),
-    host: str | None = typer.Option(None, help="The host to expose your documentation on (defaults to 127.0.0.1)"),
-    modules: list[str] | None = opt_modules,
+    port: Optional[int] = typer.Option(None, help="The port to expose your documentation on (defaults to: 8000)"),
+    host: Optional[str] = typer.Option(None, help="The host to expose your documentation on (defaults to 127.0.0.1)"),
+    modules: Optional[List[str]] = opt_modules,
     reload: bool = typer.Option(False, help="If true the server will live load any changes"),
 ) -> None:
     """Open your default web browser to a locally started development webserver."""
@@ -158,13 +159,13 @@ app.command(name="in-browser")(in_browser)
 def on_github_pages(
     directory: str = typer.Argument("", help="The root folder of your project."),
     config_file: str = typer.Option("pyproject.toml", help="The TOML formatted config file you wish to use."),
-    message: str | None = typer.Option(None, help="The commit message to use when uploading your documentation."),
+    message: Optional[str] = typer.Option(None, help="The commit message to use when uploading your documentation."),
     force: bool = typer.Option(False, help="Force the push to the repository."),
     ignore_version: bool = typer.Option(
         False,
         help="Ignore check that build is not being deployed with an old version.",
     ),
-    modules: list[str] | None = opt_modules,
+    modules: Optional[List[str]] = opt_modules,
 ) -> None:
     """Regenerates and deploys the documentation to GitHub pages."""
     api.on_github_pages(
